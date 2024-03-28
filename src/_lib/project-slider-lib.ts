a0ac9711e0d4b68f000260ab90b9f4ab63428fba
Mark Kevin J. Sandoval <sandoval.markkevin.j@gmail.com>
2024-03-28 11:41:18 +0800

    added new projects

--- src/_lib/project-slider-lib.ts
+++ src/_lib/project-slider-lib.ts
@@ -7,4 +7,16 @@ export const projectSlider: ProjectSliderInterface[] = [
     slider_project_title: 'Stardust Threads',
     slider_project_link: 'https://stardust-threads.vercel.app/',
   },
+  {
+    slider_id: 2,
+    slider_webm: './webms/project-2.webm',
+    slider_project_title: 'Pack it up',
+    slider_project_link: 'https://pack-it-up.vercel.app/',
+  },
+  {
+    slider_id: 3,
+    slider_webm: './webms/project-3.webm',
+    slider_project_title: 'RumbleRoll',
+    slider_project_link: 'https://rumbleroll.vercel.app/',
+  },
 ];
